d2abfa26423e190c29e161f53b22fc0750be47e2
lamsd <huynhlam14091994@gmail.com>
2020-07-22 12:22:39 +0700

    Value

--- main.ts
+++ main.ts
@@ -1427,9 +1427,8 @@ namespace Demo4 {
     }
 
 
-    let _temperature: number = -999.0
-    let _humidity: number = -999.0
-    let _readSuccessful: boolean = false
+    let _temperature: number = -999.0;
+    let _humidity: number = -999.0;
 
     // /**
     // * Query data from DHT11/DHT22 sensor. If you are using 4 pins/no PCB board versions, you'll need to pull up the data pin. 
@@ -1537,7 +1536,7 @@ namespace Demo4 {
     //     if (wait) basic.pause(2000)
 
     // }
-    //    
+       
     //  /**
     // * Read humidity/temperature data from lastest query of DHT11/DHT22
     // */
@@ -1565,9 +1564,7 @@ namespace Demo4 {
         for (let index = 0; index < 40; index++) dataArray.push(false)
         for (let index = 0; index < 5; index++) resultArray.push(0)
         _humidity = -999.0
-        _temperature = -999.0
-        _readSuccessful = false
-
+        _temperature = -999.0;
         startTime = input.runningTimeMicros()
         let dataPin;
         switch (port) {
@@ -1608,54 +1605,26 @@ namespace Demo4 {
         //verify checksum
         checksumTmp = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3]
         checksum = resultArray[4]
-        if (checksumTmp >= 512) checksumTmp -= 512
-        if (checksumTmp >= 256) checksumTmp -= 256
-        if (checksum == checksumTmp) _readSuccessful = true
+        if (checksumTmp >= 512) checksumTmp -= 512;
+        if (checksumTmp >= 256) checksumTmp -= 256;
 
-        //read data if checksum ok
-        if (_readSuccessful) {
-            if (DHT == DHTtype.DHT11) {
-                //DHT11
-                _humidity = resultArray[0] + resultArray[1] / 100
-                _temperature = resultArray[2] + resultArray[3] / 100
-            } else {
-                //DHT22
-                let temp_sign: number = 1
-                if (resultArray[2] >= 128) {
-                    resultArray[2] -= 128
-                    temp_sign = -1
-                }
-                _humidity = (resultArray[0] * 256 + resultArray[1]) / 10
-                _temperature = (resultArray[2] * 256 + resultArray[3]) / 10 * temp_sign
+        //read data if checksum ok;
+        if (DHT == DHTtype.DHT11) {
+            //DHT11
+             _humidity = resultArray[0] + resultArray[1] / 100;
+             _temperature = resultArray[2] + resultArray[3] / 100;
+        } else {
+            //DHT22
+            let temp_sign: number = 1
+            if (resultArray[2] >= 128) {
+                resultArray[2] -= 128
+                temp_sign = -1
             }
+            _humidity = (resultArray[0] * 256 + resultArray[1]) / 10
+            _temperature = (resultArray[2] * 256 + resultArray[3]) / 10 * temp_sign;
         }
-
-        //serial output
-        if (serialOtput) {
-            let DHTstr: string = ""
-            if (DHT == DHTtype.DHT11) DHTstr = "DHT11"
-            else DHTstr = "DHT22"
-            serial.writeLine(DHTstr + " query completed in " + (endTime - startTime) + " microseconds")
-            if (_readSuccessful) {
-                serial.writeLine("Checksum ok")
-                serial.writeLine("Humidity: " + _humidity + " %")
-                serial.writeLine("Temperature: " + _temperature + " *C")
-            } else {
-                serial.writeLine("Checksum error")
-            }
-            serial.writeLine("----------------------------------------")
-        }
-        // //wait 2 sec after query if needed
+        // //wait 2 sec after query if needed;
         // if (wait) basic.pause(2000)
-        return data == dataType.humidity ? _humidity : _temperature
-    }
-
-    // /**
-    // * Determind if last query is successful (checksum ok)
-    // */
-    // //% block="Last query successful?" group="3. Sensor."  advanced=true
-    // export function readDataSuccessful(): boolean {
-    //     return _readSuccessful
-    // }
-
+        return dataType.humidity ? _humidity : _temperature
+    };
 }
